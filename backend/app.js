606f7f567c0c90d01811ca9472ba169036789a90
gonzalezraul <rgonzaleza@alumnes.santjosepobrer.es>
2025-02-27 16:21:46 +0100

    añadido añadir info

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('¡Hola, mundo desde Node.js!');
 });
 
+app.get('/añadir_info', (req, res) => {
+    res.send();
+});
+
 // Iniciar el servidor
 const PORT = 5000;
 app.listen(PORT, () => {
