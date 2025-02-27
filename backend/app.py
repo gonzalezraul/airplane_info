d105d23e225c1d4790e740a25ad078a0776ab997
gonzalezraul <rgonzaleza@alumnes.santjosepobrer.es>
2025-02-27 17:18:39 +0100

    minor changes

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+@app.route("/")
+def main():
+    app.config["DEBUG"] = True
+    app.run()
+
 
 @app.route('/inicio')
 def inicio():
@@ -8,4 +14,4 @@ def inicio():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
